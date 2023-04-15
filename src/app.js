535f147923420326425d8d0c2283606fdc2dbebc
Bex <rebeccaokine.na@gmail.com>
2023-04-15 04:57:38 +0000

    Added background gif, a form and edited the form

--- src/app.js
+++ src/app.js
@@ -2,13 +2,21 @@ function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   let minutes = date.getMinutes();
-  if (minutes < 10 ){
+  if (minutes < 10) {
     minutes = `0${minutes}`;
   }
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
@@ -20,6 +28,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#dateTime");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -29,6 +38,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "e6fdbebba6d6f76e9a50f7444eca5b52";
+let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
