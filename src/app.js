e4e58c6bcd1c2ec32767a4e218d6384015b8b201
Bex <rebeccaokine.na@gmail.com>
2023-04-15 13:02:32 +0000

    Added a search engine

--- src/app.js
+++ src/app.js
@@ -37,8 +37,19 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
-let apiKey = "e6fdbebba6d6f76e9a50f7444eca5b52";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+function search(city){
+    let apiKey = "e6fdbebba6d6f76e9a50f7444eca5b52";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("Accra");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit",handleSubmit);
