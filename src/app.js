3548d02a2139edcae0478150455d7e65285a1c9c
Bex <rebeccaokine.na@gmail.com>
2023-04-19 17:28:08 +0000

    Fixed weather forecast not showing

--- src/app.js
+++ src/app.js
@@ -51,43 +51,34 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast(response) {
-  console.log(response.data.daily);
+function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-
+  let days = ["Thu", "Fri", "Sat", "Sun"];
   let forecastHTML = `<div class="row">`;
+
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
       `
-              <div class="col-2">
+              <div class="col-2" >
                 <div class="weather-forecast-date">${day}</div>
                 <img
                   src="http://openweathermap.org/img/wn/03n@2x.png"
                   alt="weather icon"
                   width="50px"
                 />
-                <div class="weather-forcast-temperature">
+                <div class="weather-forecast-temperature">
                   <span class="weather-forecast-temp-max">15°</span>
                   <span class="weather-forecast-temp-min">12° </span>
                 </div>
-              </div>
-            `;
+              </div>          
+        `;
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
-}
-
-function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "e6fdbebba6d6f76e9a50f7444eca5b52";
-  let apiUrl =
-    `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={part}&appid=${apiKey}&unit=metric`;
-    console.log(apiUrl);
 }
 
 function displayTemperature(response) {
@@ -113,8 +104,6 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   displayBackgroundImage(response.data.weather[0].description);
-
-  getForecast(response.data.coord);
 }
 
 function search(city) {
